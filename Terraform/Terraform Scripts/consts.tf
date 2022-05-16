6df7ee3a5a6e4bffe11abca6d4af3d556301061e
Michal Pierzchalski <m.pierzchalski@cloudyrion.com>
2022-05-16 12:58:19 +0200

    Added terraform scripts

---
+++ Terraform/Terraform Scripts/consts.tf
@@ -0,0 +1,50 @@
+
+
+data "aws_caller_identity" "current" {}
+
+variable "company_name" {
+  default = "acme"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+locals {
+  resource_prefix = {
+    value = "${data.aws_caller_identity.current.account_id}-${var.company_name}-${var.environment}"
+  }
+}
+
+
+
+variable "profile" {
+  default = "default"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable ami {
+  type    = string
+  default = "ami-0b0ea68c435eb488d"
+}
+
+variable "dbname" {
+  type        = string
+  description = "Name of the Database"
+  default     = "db1"
+}
+
+variable "password" {
+  type        = string
+  description = "Database password"
+  default     = "Aa1234321Bb"
+}
+
+variable "neptune-dbname" {
+  type        = string
+  description = "Name of the Neptune graph database"
+  default     = "neptunedb1"
+}
